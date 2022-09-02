495185a8e323c29bf32de929725a323e43ed4f72
rsonghuster <ls_huster@163.com>
2022-09-02 12:00:44 +0800

    add width and height in headless ffmpeg

--- headless-ffmpeg/src/code/record.js
+++ headless-ffmpeg/src/code/record.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer-core');
 var args = process.argv.splice(2)
 console.log(args);
 
-async function test(){
+async function record(){
   const browser = await puppeteer.launch(
     {
       headless: false,
@@ -13,24 +13,32 @@ async function test(){
         '--autoplay-policy=no-user-gesture-required',
         '--enable-usermedia-screen-capturing',
         '--allow-http-screen-capture',
-        "--disable-gpu",
+        '--disable-gpu',
         '--start-fullscreen',
-        '--window-size=1080,720',
+        '--window-size='+args[2],
       ],
       ignoreDefaultArgs: ['--mute-audio', '--enable-automation']
     });
   console.log("try new page .....");
   const page = await browser.newPage();
+  const li = args[2].split(',');
+  console.log(li)
+  const w = parseInt(li[0], 10)
+  const h = parseInt(li[1], 10)
+  await page.setViewport({
+    width: w,
+    height: h,
+  });
   console.log("try goto .....");
   url = args[1] || "http://dy-vedio.oss-cn-hangzhou.aliyuncs.com/video/a.mp4";
   await page.goto(url);
   var timeout = parseInt(args[0], 10)*1000;
   console.log("waitFor begin .....");
-  await page.waitFor(timeout);
+  await page.waitForTimeout(timeout);
   // console.log("screenshot .....");
   // await page.screenshot({ path: '/var/output/test.png' });
   await browser.close();
   console.log("browser closed ...........");
 }
 
-test();
+record();
--- headless-ffmpeg/src/code/server.js
+++ headless-ffmpeg/src/code/server.js
@@ -7,34 +7,6 @@ const REQUEST_ID_HEADER = 'x-fc-request-id'
 
 var execSync = require("child_process").execSync;
 const OSS = require('ali-oss');
-
-const buffDeltaTime = 30000;
-var env = Object.assign({}, process.env);
-// env.stdio = 'inherit'
-function myExecSync(cmd, t) {
-  var start = new Date().getTime(); // 开始时间
-  // console.log(JSON.stringify(env, null, 2))
-  try{
-    execSync(cmd, {
-      cwd: process.cwd(),
-      env: env,
-      shell: "/bin/bash",
-      timeout: t,
-      stdio: 'inherit'
-    });
-  } catch (e) {
-    var end = new Date().getTime(); // 结束时间
-    var totalTime = end - start;
-    if(totalTime >= t - buffDeltaTime){
-      // 这个时候，录屏 shell 脚本执行完毕，可以忽视退出的 15 错误
-      console.log("******** myExecSync success **********");
-    } else {
-      console.log(e.toString());
-      throw new Error(e.toString());
-    }
-  }
-}
-
 const express = require('express');
 const app = express();
 app.use(express.raw());
@@ -52,9 +24,11 @@ app.post('/invoke', (req, res) => {
     var recordTime = evt["record_time"];
     var videoUrl = evt["video_url"];
     var outputFile = evt["output_file"];
-    var cmdStr = "bash record.sh " + recordTime + " " + videoUrl;
-    console.log(cmdStr);
-    myExecSync(cmdStr, parseInt(recordTime, 10)*1000+buffDeltaTime);
+    var width = evt["width"];
+    var height = evt["height"];
+    var cmdStr = `/code/record.sh ${recordTime} ${videoUrl} ${width}x${height}x24 ${width},${height} ${width}x${height}`;
+    console.log(`cmd is ${cmdStr} \n`);
+    execSync(cmdStr, {stdio: 'inherit', shell: "/bin/bash"});
     console.log("start upload video to oss ...");
     const store = new OSS({
       accessKeyId: process.env.OSS_AK_ID,
@@ -64,18 +38,18 @@ app.post('/invoke', (req, res) => {
     });
     store.put(outputFile, '/var/output/test.mp4').then((result) => {
       console.log("finish to upload video to oss");
-      myExecSync("rm -rf /var/output/test.mp4", 5000);
+      execSync("rm -rf /var/output/test.mp4", {stdio: 'inherit'});
       res.send('OK');
       console.log(`FC Invoke End RequestId: ${rid}`)
     }).catch(function (e) {
       console.log("fail to upload video to oss: " + e.toString())
       res.status(404).send(e.stack || e);
-      console.log(`FC Invoke End RequestId: ${rid}`);
+      console.log(`FC Invoke End RequestId: ${rid}, Error: Unhandled function error`);
     });
   } catch (e) {
     console.error(e.stack || e);
     res.status(404).send(e.stack || e);
-    console.log(`FC Invoke End RequestId: ${rid}`)
+    console.log(`FC Invoke End RequestId: ${rid}, Error: Unhandled function error`)
   }
 });
 
